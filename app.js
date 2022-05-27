0d171e5072f2c5a28acbfea93e74fd3d38fda3a8
Gabriel <gdbianchii@gmail.com>
2022-05-27 16:41:01 -0300

    Testing out insert into using json

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 
 var express = require('express');
+var app = express();
 var path = require('path');
 
 
 var indexRouter = require('./routes/index');
 var leadTrackRouter = require('./routes/leadTrack');
 
-var app = express();
+
 
 // view engine setup
-
 app.set('view engine', 'pug');
 
 app.use('/', indexRouter);
--- routes/leadTrack.js
+++ routes/leadTrack.js
@@ -1,13 +1,33 @@
 var express = require('express');
-const { compileClientWithDependenciesTracked } = require('pug');
 var router = express.Router();
 const pool = require('./db')
+var bodyParser = require('body-parser');
+router.use(bodyParser.json());
+
 
 router.get('/', function (req, res) {
-    pool.query('Select * from "public"."Lead_Track"', (err, result) => {
+    pool.query('Select * from lead_track', (err, result) => {
         if (err) throw err
         res.json(result.rows)
     })
+});
+
+router.post('/json', (req, res) => {
+    console.log('yee')
+    console.log(req.body)
+    res.send(req.body);    // echo the result back
+});
+
+router.get('/new/:name/:email/:tag_site/:tag/:city/:phone_number/:utm_souce/:utm_campaign/:utm_content', function (req, res) {
+    var queryString = "Insert into lead_track values (uuid_generate_v4(), Now(), '" + req.params.name + "', '" + req.params.email + "', '" + req.params.tag_site + "', '" +
+        req.params.tag + "', '" + req.params.city + "', " + req.params.phone_number + ", " + req.params.utm_souce + ", " + req.params.utm_campaign + ", " + req.params.utm_content + ")"
+
+    pool.query(queryString, (err, result) => {
+        if (err) throw err
+        res.send('Lead inserted successfuly')
+        //res.json(result.rows)
+    })
+
 });
 
 
