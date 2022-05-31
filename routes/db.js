377c953b91ef7555ab4db98551c4887ebfe6e448
Gabriel <gdbianchii@gmail.com>
2022-05-31 10:22:30 -0300

    Melhorias db.js

--- routes/db.js
+++ routes/db.js
@@ -9,55 +9,67 @@
 //});
 //module.exports = pool;
 const uuid = require('uuid')
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.POSTGREKODIXCONNECTIONSTRING,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
 
 async function connect() {
-    if (global.connection)
-        return global.connection.connect()
-
-    const { Pool } = require('pg');
-    const pool = new Pool({
-        connectionString: process.env.POSTGREKODIXCONNECTIONSTRING,
-        ssl: {
-            rejectUnauthorized: false,
-        },
-    });
-
-    //apenas testando a conexão
-    const client = await pool.connect()
-    console.log("Criou pool de conexões no PostgreSQL!")
-    client.release();
-
-    //guardando para usar sempre o mesmo
-    global.connection = pool;
-    return pool.connect()
+    //if (global.connection) {
+    //    return global.connection.connect()
+    //}
+    //
+    ////apenas testando a conexão
+    //const client = await pool.connect()
+    //console.log("Criou pool de conexões no PostgreSQL!")
+    //client.release();
+    //
+    ////guardando para usar sempre o mesmo
+    //global.connection = pool;
+    //return pool.connect()
+    pool.on('connect', () => {
+        console.log('Conectado ao PostgreSQL')
+    })
 }
 
 async function selectUsers() {
-    const client = await connect()
-    const res = await client.query('SELECT * FROM users')
+
+
+
+    const res = await pool.query('SELECT * FROM users')
+    //const client = await connect(){}
+    //const res = await client.query('SELECT * FROM users')
     return res.rows
 }
 
 async function selectRoles() {
-    const client = await connect()
-    const res = await client.query('Select * from user_roles')
-    return res.rows
+    try {
+        connect()
+        const res = await pool.query('Select * from user_roles')
+        return res.rows
+    } catch (err) {
+        console.log(err)
+    }
+
 }
 
 async function registerUser(login, hashedPassword, role) {
-    const client = await connect()
+    connect()
     var generateduuid = uuid.v4()
     //Insert into users
-    await client.query(`INSERT INTO users VALUES ('${generateduuid}', '${login}', '${hashedPassword}')`)
+    await pool.query(`INSERT INTO users VALUES ('${generateduuid}', '${login}', '${hashedPassword}')`)
     //Insert into userRole
     var query = `INSERT INTO user_Roles VALUES ('${generateduuid}', '${role}')`
-    await client.query(query)
+    await pool.query(query)
 }
 
 async function insertLeadTrack(name, email, tag_site, tag, city, phone_number, utm_source, utm_campaign, utm_content) {
-    const client = await connect();
+    connect();
     var query = `INSERT INTO lead_track VALUES (uuid_generate_v4(), Now(), ${name == null ? 'null' : "'" + name + "'"}, ${email == null ? 'null' : "'" + email + "'"}, ${tag_site == null ? 'null' : "'" + tag_site + "'"}, ${tag == null ? 'null' : "'" + tag + "'"}, ${city == null ? 'null' : "'" + city + "'"}, ${phone_number == null ? 'null' : "'" + phone_number + "'"}, ${utm_source == null ? 'null' : "'" + utm_source + "'"}, ${utm_campaign == null ? 'null' : "'" + utm_campaign + "'"}, ${utm_content == null ? 'null' : "'" + utm_content + "'"})`
-    await client.query(query)
+    await pool.query(query)
 }
 
 module.exports = { selectUsers, selectRoles, registerUser, insertLeadTrack }
